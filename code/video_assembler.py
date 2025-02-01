dccda78e85b774e8a7e6d363e60970295dfd7045
BaronBrabant <benny@saranne.be>
2025-02-01 14:28:04 +0000

    adds code to stick images together

--- code/video_assembler.py
+++ code/video_assembler.py
@@ -1,17 +1,34 @@
-from moviepy.editor import AudioFileClip, ImageClip
-import numpy as np
+from moviepy.editor import AudioFileClip, ImageClip, concatenate_videoclips
 
+image_files = [
+    "data_output/packages/d8498acc-a632-4d1b-8938-2448fc4fd17c/images/image_0.png",
+    "data_output/packages/d8498acc-a632-4d1b-8938-2448fc4fd17c/images/image_1.png",
+    # Add more image paths here
+]
 
-# Import the audio(Insert to location of your audio instead of audioClip.mp3)
-audio = AudioFileClip("data_output/packages/d8498acc-a632-4d1b-8938-2448fc4fd17c/audio/audio_0.mp3")
-# Import the Image and set its duration same as the audio (Insert the location of your photo instead of photo.jpg)
-clip = ImageClip("data_output/packages/d8498acc-a632-4d1b-8938-2448fc4fd17c/images/image_0.png").set_duration(audio.duration)
-# Set the audio of the clip
-clip = clip.set_audio(audio)
-# Export the clip
+audio_files = [
+    "data_output/packages/d8498acc-a632-4d1b-8938-2448fc4fd17c/audio/audio_0.mp3",
+    "data_output/packages/d8498acc-a632-4d1b-8938-2448fc4fd17c/audio/audio_1.mp3",
+    # Add more audio paths here
+]
 
-clip.write_videofile("video.mp4", fps=24)
+clips = []
 
+for img_path, aud_path in zip(image_files, audio_files):
+    # Load the audio clip
+    audio = AudioFileClip(aud_path)
+    # Create an image clip with the duration set to the duration of the audio
+    clip = ImageClip(img_path).set_duration(audio.duration)
+    # Set the audio for the image clip
+    clip = clip.set_audio(audio)
+    # Optionally, you can add some transition effects here
+    clips.append(clip)
+
+# Concatenate all the clips together
+final_clip = concatenate_videoclips(clips)
+
+# Write the final video file
+final_clip.write_videofile("video.mp4", fps=24)
 
 
 
